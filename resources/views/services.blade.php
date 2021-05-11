d59034baa2f04782f416aa924862c477eb0e3a4d
Andrea Marangon <marangonandrea84@gmail.com>
2021-05-11 15:53:30 +0200

    Esercizio terminato

---
+++ resources/views/services.blade.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Laravel</title>
+
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+  <!-- Styles -->
+  <style>
+      html, body {
+          background-color: #fff;
+          color: #636b6f;
+          font-family: 'Nunito', sans-serif;
+          font-weight: 200;
+          height: 100vh;
+          margin: 0;
+      }
+
+      .full-height {
+          height: 100vh;
+      }
+
+      .flex-center {
+          display: flex;
+          justify-content: center;
+      }
+
+      .content {
+          text-align: center;
+      }
+
+      .title {
+          font-size: 84px;
+      }
+
+      .links > a {
+          color: #636b6f;
+          padding: 0 25px;
+          font-size: 13px;
+          font-weight: 600;
+          letter-spacing: .1rem;
+          text-decoration: none;
+          text-transform: uppercase;
+      }
+
+      .m-b-md {
+          margin-bottom: 30px;
+      }
+  </style>
+</head>
+<body>
+  <div class="flex-center full-height">
+      <div class="content">
+          <div class="title m-b-md">
+              Laravel
+          </div>
+          <div class="links">
+              <a href="/">Home</a>
+              <a href="/about">About</a>
+              <a href="/services">Servives</a>
+              <a href="/showcase">Showcase</a>
+              <a href="/blog">Blog</a>
+              <a href="/contact">Contact</a>
+          </div>
+          <div class="title m-b-md">
+              SERVICES
+          </div>
+
+      </div>
+  </div>
+
+</body>
+</html>
